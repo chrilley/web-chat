b8594784e9ce8219f35ccddd9dd8de56c4b7b108
Christian Yngvesson <chrille_y@hotmail.com>
2022-09-16 18:23:42 +0200

    .Net backend project

---
+++ ChatService/ChatService/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatService.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string _BotUser;
+
+        public ChatHub()
+        {
+            _BotUser = "MyChat Bot";
+        }
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage",_BotUser, $"{userConnection.User} has joined {userConnection.Room}");
+        }
+    }
+}
